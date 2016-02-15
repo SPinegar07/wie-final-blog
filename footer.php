ca999799394c467761d9653edcdcd3542f87de0c
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-02-14 19:55:22 -0600

    formatted footer

---
+++ footer.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>MCJ6213 Template</title>
+	<!-- Links to our Style.css file -->
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('style.css'); ?>" />
+</head>
+<body>
+            <footer class="row">
+                <div class="twelve columns">
+                    <p>Footer content goes here.</p>
+                </div>
+            </footer>
+</div>
+</body>
+
+</html>
