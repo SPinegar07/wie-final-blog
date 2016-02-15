bd135a9d690f45274249e4869cd51f03143ab97d
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-02-14 19:52:36 -0600

    formatted header

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>MCJ6213 Template</title>
+	<!-- Links to our Style.css file -->
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('style.css'); ?>" />
+</head>
+<body>
+	<div class="container">
+            <header class="row">
+                <div class="twelve columns">
+                    <h1>Blank WordPress Template</h1>
+                    <h2>This is my WordPress template.</h2>
+                </div>
+            </header>
+</div>
+</body>
+
+</html>
