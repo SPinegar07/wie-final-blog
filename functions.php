9ef20bb8bf1a094bb517c3c2a72255a8cf888c72
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-02-14 21:09:25 -0600

    added additional widget functionality

--- functions.php
+++ functions.php
@@ -11,6 +11,37 @@ function blank_widgets_init() {
         'before_title' => '<h2>',
         'after_title' => '</h2>'                        
         ));
-    }
+/*--- New Widget --- */
+    register_sidebar( array(
+        'name' => ('Footer Widget One'),
+        'id' => 'footer-widget-one',
+        'description' => 'First widget for our footer', 
+        'before_widget' => '<div class="widget-footer">', 
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'                        
+        ));
+    /*--- Second New Widget --- */
+    register_sidebar( array(
+        'name' => ('Footer Widget Two'),
+        'id' => 'footer-widget-two',
+        ''description' => 'Second widget for our footer', 
+        'before_widget' => '<div class="widget-footer">', 
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'                        
+        ));
+    /*--- Third New Widget --- */
+    register_sidebar( array(
+        'name' => ('Footer Widget Three'),
+        'id' => 'footer-widget-three',
+        'description' => 'Third widget for our footer', 
+        'before_widget' => '<div class="widget-footer">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2>',
+        'after_title' => '</h2>'                        
+        ));
+
+}
 add_action('widgets_init', 'blank_widgets_init');
 ?>
