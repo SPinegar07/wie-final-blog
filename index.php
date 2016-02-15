afea532d0c517b79e2bed82df875c3824fc1bf17
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-02-14 19:14:57 -0600

    create blank index and style page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>MCJ6213 Template</title>
+</head>
+<body>
+    <h1>Blank WordPress Template</h1>
+    <p>This is my WordPress template.</p>
+</body>
+
+</html>
