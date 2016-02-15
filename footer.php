34e04611f11dc21c911ef6218c7b7632b19c1bd3
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-02-14 21:12:16 -0600

    added widget areas to footer

--- footer.php
+++ footer.php
@@ -8,12 +8,18 @@
 </head>
 <body>
             <footer class="row">
-                <div class="twelve columns">
-                    <p>Footer content goes here.</p>
-                </div>
-            </footer>
+    <div class="four columns">
+<?php dynamic_sidebar('footer-widget-one'); ?>
 </div>
+    <div class="four columns">
+<?php dynamic_sidebar('footer-widget-two'); ?>
+</div>
+    <div class="four columns">
+<?php dynamic_sidebar('footer-widget-three'); ?>
+</div>
+</footer>
+</div> 
 <?php wp_footer(); ?>
-</body>
 
+</body>
 </html>
