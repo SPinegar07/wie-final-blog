fff9ee32e6b251eb2e72f9dc5e5c7fe913109dbe
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-02-14 20:51:42 -0600

    added sidebar setup

--- page.php
+++ page.php
@@ -10,16 +10,22 @@
 	<div class="container">
 		<?php get_header(); ?>
             <section class="row">
-                <div class="twelve columns">
-                    <!-- BEGIN PAGE PHP -->
-                    <?php if (have_posts()) : 
-                        /* OUR DATA CONTEXT IS DEFINED  */      
-                        while (have_posts()) : the_post(); ?> 
-                            <h2><?php the_title(); ?></h2>
-                            <?php the_content();
-                        endwhile;
-                    endif; ?>
-                    <!-- END PAGE PHP -->
+                <!-- MAKE THIS COLUMN NINE WIDTH -->
+                    <div class="nine columns">
+                        <?php if (have_posts()) :           
+                            while (have_posts()) : the_post(); ?> 
+                                <h2><?php the_title(); ?></h2>
+                                <?php the_content();
+                            endwhile;
+                        endif; ?>
+                    </div>
+            <!-- ADD IN A THREE WIDTH COLUMN - COMPLETE THE TWELVE REQUIREMENT WIDTH -->
+            <!-- BEGIN SIDEBAR -->
+                    <div class="three columns">
+                        <h3>Sidebar Text</h3>
+                        <p>This is the sidebar content to put in here.</p>
+                    </div>
+            <!-- END SIDEBAR -->
                 </div>
             </section>
         <?php get_footer(); ?>
