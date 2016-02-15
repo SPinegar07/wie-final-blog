c07da0654e2cd7357a9131553e14bc46f1a26558
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-02-14 19:34:34 -0600

    added skeleton grid and link to skeleton.css

--- index.php
+++ index.php
@@ -3,10 +3,28 @@
 <head>
 	<meta charset="UTF-8">
 	<title>MCJ6213 Template</title>
+	<!-- Links to our Style.css file -->
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('style.css'); ?>" />
 </head>
 <body>
-    <h1>Blank WordPress Template</h1>
-    <p>This is my WordPress template.</p>
+	<div class="container">
+            <header class="row">
+                <div class="twelve columns">
+                    <h1>Blank WordPress Template</h1>
+                    <p>This is my WordPress template.</p>
+                </div>
+            </header>
+            <section class="row">
+                <div class="twelve columns">
+                    <p>Body content goes here.</p>
+                </div>
+            </section>
+            <footer class="row">
+                <div class="twelve columns">
+                    <p>Footer content goes here.</p>
+                </div>
+            </footer>
+</div>
 </body>
 
 </html>
