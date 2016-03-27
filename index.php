d68f9b75ec00f6ee0ea8f5610c0131fadc8459ec
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-03-27 18:26:57 -0500

    inital commit

--- index.php
+++ index.php
@@ -1,43 +1,30 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="UTF-8">
-	<title>MCJ6213 Template</title>
-		<!-- Links to our Style.css file -->
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('style.css'); ?>" />
-</head>
-<body>
-	<div class="container">
-		<?php get_header(); ?>
+    <head>
+        <title></title>
+        <!-- Links to our Style.css file -->
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('style.css'); ?>" />
+    </head>
+    <body>
+        <div class="container">
+            <header class="row">
+                <div class="twelve columns">
+                    <h1>Blank WordPress Template</h1>
+                    <p>This is my WordPress template.</p>
+                </div>
+            </header>
+            <!-- END OF HEADER - BEGIN SECTION CONTENT -->
             <section class="row">
                 <div class="twelve columns">
-                	<?php 
-				        if ( have_posts() ) {
-				        while ( have_posts() ) {
-				            the_post();?>
-				            <h3><?php the_title(); ?></h3>
-				            <?php 
-				            if ( has_post_thumbnail() ) {
-				                the_post_thumbnail('thumbnail');
-				            }
-				            the_excerpt(); 
-				                } // end while
-				            } // end if
-					            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-					            <?php 
-					            if ( has_post_thumbnail() ) {
-					                the_post_thumbnail('thumbnail');
-					            }
-					            the_excerpt(); ?>
-					    <p><a href="<?php the_permalink(); ?>">Read More</a></p>
-<?php } // end while
-            } // end if
-        ?>
-				        ?>
+                    <p>Body content goes here.</p>
                 </div>
             </section>
-        <?php get_footer(); ?>
-</div>
-</body>
-
+            <!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
+            <footer class="row">
+                <div class="twelve columns">
+                    <p>Footer content goes here.</p>
+                </div>
+            </footer>
+        </div> <!-- ends container -->
+    </body>
 </html>
