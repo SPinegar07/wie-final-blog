f48cf920a70e41d7698208d313e2ec23a9ed30f0
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-03-27 18:33:52 -0500

    updated index page with loop

--- index.php
+++ index.php
@@ -6,10 +6,19 @@
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('style.css'); ?>" />
     </head>
     <body>
-        <?php get_header(); ?>
+        <div class="container">
+            <?php get_header(); ?>
     <section class="row">
         <div class="twelve columns">
-            <p>Body content goes here.</p>
+<!-- BEGIN LOOP -->
+            <?php 
+            if ( have_posts() ) {
+                while ( have_posts() ) {
+                    the_post(); 
+                } // end while
+            } // end if
+            ?>
+<!-- END LOOP -->
         </div>
     </section>
 <?php get_footer(); ?>
