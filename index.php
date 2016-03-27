4a3c264dcb730432c75836e16172bf4d0951cfce
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-03-27 18:30:16 -0500

    updated header and footer to connect to external files

--- index.php
+++ index.php
@@ -6,25 +6,13 @@
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('style.css'); ?>" />
     </head>
     <body>
-        <div class="container">
-            <header class="row">
-                <div class="twelve columns">
-                    <h1>Blank WordPress Template</h1>
-                    <p>This is my WordPress template.</p>
-                </div>
-            </header>
-            <!-- END OF HEADER - BEGIN SECTION CONTENT -->
-            <section class="row">
-                <div class="twelve columns">
-                    <p>Body content goes here.</p>
-                </div>
-            </section>
-            <!-- BEGINNING OF FOOTER - END SECTION CONTENT -->
-            <footer class="row">
-                <div class="twelve columns">
-                    <p>Footer content goes here.</p>
-                </div>
-            </footer>
+        <?php get_header(); ?>
+    <section class="row">
+        <div class="twelve columns">
+            <p>Body content goes here.</p>
+        </div>
+    </section>
+<?php get_footer(); ?>
         </div> <!-- ends container -->
     </body>
 </html>
