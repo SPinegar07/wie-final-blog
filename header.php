3e6504438445697c947f87d4c7ef267ada6fa9e9
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-03-28 11:39:52 -0500

    added logo, removed search and menu

--- header.php
+++ header.php
@@ -5,29 +5,21 @@
 	<title>MCJ6213 Template</title>
     <?php wp_head(); ?>
 	<!-- Links to our Style.css file -->
-    <link rel="stylesheet" type="text/css" href="<?php bloginfo('style.css'); ?>" />
+    <!-- Sylesheet -->
+        <style type="text/css">
+  <?php
+    include("style.css");
+  ?>
+</style>
 </head>
 <body>
 	<div class="container">
             <header class="row">
                 <div class="twelve columns">
                     <!-- ADD LINK TO HOME_URL -->
-                    <h1><a href="<?php $url = home_url('/'); echo $url; ?>"><?php bloginfo('name'); ?></a></h1>
-                    <p><?php bloginfo('description'); ?></p>
-                </div>
-                <!-- Add Search Form -->
-                <div class="three columns">
-                    <?php get_search_form(); ?>
+                    <a href="index.php"><img src="images/logo.png" alt="logo"></a>
                 </div>
                         </header>
-            <!-- Add Menu Here -->
-            <div class="row">
-                <div class="twelve columns">
-                    <?php wp_nav_menu(array(
-                        'sort_column' => 'menu_order', 
-                        'container_class' => 'blank-menu-header'
-                        ));?>
-                </div>
 </div>
 </div>
 </body>
