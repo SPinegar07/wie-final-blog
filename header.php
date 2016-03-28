1d4bae44b8520b7d2da368d069c3cc34e1b36cce
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-03-28 12:58:03 -0500

    formatted header with social logos and main logo

--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-	<title>MCJ6213 Template</title>
+    <meta charset="UTF-8">
+    <title>MCJ6213 Template</title>
     <?php wp_head(); ?>
-	<!-- Links to our Style.css file -->
+    <!-- Links to our Style.css file -->
     <!-- Sylesheet -->
         <style type="text/css">
   <?php
@@ -13,11 +13,27 @@
 </style>
 </head>
 <body>
-	<div class="container">
+    <div class="container">
             <header class="row">
-                <div class="twelve columns">
+                <div class="nine columns columns">
                     <!-- ADD LINK TO HOME_URL -->
-                    <a href="index.php"><img src="images/logo.png" alt="logo"></a>
+                    <a href="http://www.kittensunicornsandtacos.com/UFL"><img src="http://www.kittensunicornsandtacos.com/UFL/wp-content/uploads/2016/03/logo.png" width="200" alt="logo"></a>
+                </div>
+                <div class="three columns">
+                        <table width="200">
+                            <tr>
+                                <td>
+                                    <a href="https://www.instagram.com/"><img src="http://www.kittensunicornsandtacos.com/UFL/wp-content/uploads/2016/03/instagram.png" width="40" alt="logo"></a>
+                                </td>
+                                <td>
+                                    <a href="https://www.facebook.com/"><img src="http://www.kittensunicornsandtacos.com/UFL/wp-content/uploads/2016/03/facebook.png" width="40" alt="logo"></a>
+                                </td>
+                                <td>
+                                    <a href="https://twitter.com/"><img src="http://www.kittensunicornsandtacos.com/UFL/wp-content/uploads/2016/03/instagram.png" width="40" alt="logo"></a>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
                         </header>
 </div>
