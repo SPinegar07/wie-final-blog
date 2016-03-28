d334fec58ddda46130284e6a4f1ccfb88f96d12c
Sylvia Dulaney <SPinegar07@ufl.edu>
2016-03-28 09:23:37 -0500

    updated with new code to pull style sheet

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         <div class="eight columns">
 <!-- BEGIN LOOP -->
             <?php if ( have_posts() ) : ?>
-            <h2>Web Design Blog</h2>
+            <h2>Fancy Blog About Web Stuffs</h2>
             <?php
             // The Loop
             while ( have_posts() ) : the_post();?>
@@ -25,22 +25,26 @@
                 <?php the_excerpt(); ?>
             <?php endwhile; ?> <!-- End Loop -->
         <?php endif; ?>
-        <?php else: ?>
-            <p>Sorry, no posts matched your criteria.</p>
-        <?php endif; ?>
-    </div>
 <!-- END LOOP -->
     </div>
     </section>
 <!-- SIDEBAR -->
     <section class="row">
         <div class="four columns">
-            <ul>
-            <?php wp_get_archives('type=monthly'); ?>
-        </ul>
-        </div>
-    </section>
-<?php get_footer(); ?>
+            <?php if ( have_posts() ) : ?>
+            <h3>Recent Posts</h3>
+            <?php
+            // The Loop
+            while ( have_posts() ) : the_post();?>
+            <!-- data context -->
+                <h2>
+                    <a href="<?php the_permalink() ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </h2>
+                <?php the_excerpt(); ?>
+            <?php endwhile; ?> <!-- End Loop -->
+        <?php endif; ?>
         </div> <!-- ends container -->
     </body>
 </html>
